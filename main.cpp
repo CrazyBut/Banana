9ff8aad4917f1f1c00617255f63779cbeef7c0c1
CrazyBut <44512731+CrazyBut@users.noreply.github.com>
2019-06-29 16:05:42 +1000

    Fixing version

--- main.cpp
+++ main.cpp
@@ -1,252 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <iostream>
+#include <stack>
+#include <string.h>
+#include <cstdlib>
+#include <list>
+#include <deque>
+#include "opz.h"
 
 using namespace std;
 
-// Иерархия мемов
-class Meme {
-public:
-    virtual string info() = 0;
-
-    virtual ~Meme() {
-    }
-};
-
-class LegendaryMeme : public Meme {
-public:
-    string info() {
-        return "LegendaryMeme";
-    };
-};
-
-class AnimeMeme : public Meme {
-public:
-    string info() {
-        return "AnimeMeme";
-    };
-};
-
-class HahaMeme : public Meme {
-public:
-    string info() {
-        return "HahaMeme";
-    };
-};
-
-class PolitMeme : public Meme {
-public:
-    string info() {
-        return "PolitMeme";
-    };
-};
-
-
-// Фабрики объектов
-class Factory {
-public:
-    virtual Meme *createMeme() = 0;
-
-    virtual ~Factory() {}
-};
-
-class LegendaryFactory : public Factory {
-public:
-    Meme *createMeme() {
-        return new LegendaryMeme;
-    }
-};
-
-class AnimeFactory : public Factory {
-public:
-    Meme *createMeme() {
-        return new AnimeMeme;
-    }
-};
-
-class HahaFactory : public Factory {
-public:
-    Meme *createMeme() {
-        return new HahaMeme;
-    }
-};
-
-class PolitFactory : public Factory {
-public:
-    Meme *createMeme() {
-        return new PolitMeme;
-    }
-};
-
-class Human {
-    string name;
-    bool isLovingLegendary;
-    bool isLovingAnime;
-    bool isLovingHaha;
-    bool isLovingPolit;
-public:
-    Human(){
-        isLovingLegendary = false;
-        isLovingAnime = false;
-        isLovingHaha = false;
-        isLovingPolit = false;
-        name = "";
-    }
-    void setLovingAnime(bool a){
-        isLovingAnime = a;
-    }
-    void setLovingHaha(bool b){
-        isLovingHaha = b;
-    }
-    void setLovingPolit(bool c){
-        isLovingPolit = c;
-    }
-    Human(bool a, bool b, bool c, string n) {
-        isLovingAnime = a;
-        isLovingHaha = b;
-        isLovingPolit = c;
-        isLovingLegendary = true;
-        name = n;
-    }
-
-    string getname(){
-        return name;
-    }
-    void setName(string NM){
-        name = NM;
-    }
-
-
-    void review(Meme &m) {
-        cout << m.info() << ": ";
-        if (m.info() == "LegendaryMeme") {
-            cout << " amazing";
-        }
-
-        if (m.info() == "AnimeMeme") {
-            if (isLovingAnime) {
-                cout << " amazing";
-            } else {
-                cout << "Nein";
-            }
-        }
-
-        if (m.info() == "HahaMeme") {
-            if (isLovingHaha) {
-                cout << " amazing";
-            } else {
-                cout << "Nein";
-            }
-        }
-
-        if (m.info() == "PolitMeme") {
-            if (isLovingPolit) {
-                cout << " amazing";
-            } else {
-                cout << "Nein";
-            }
-        }
-
-        cout << endl;
-    }
-};
-
-
-class HumanBuilder
-{
-protected:
-    Human* p;
-public:
-    HumanBuilder(): p(NULL) {}
-    void setLovingAnime(bool a){
-        p->setLovingAnime(a);
-    }
-    void setLovingHaha(bool b){
-        p->setLovingHaha(b);
-    }
-    void setLovingPolit(bool c){
-        p->setLovingPolit(c);
-    }
-    void setName (string name){
-        p->setName(name);
-    }
-     ~HumanBuilder() {}
-     void createHuman() {p=new Human;}
-     Human* getHuman() { return p; }
-
-};
-
 int main() {
-//    Human human1(true, false, false, "AnimeBoy");
-//    Human human2(true, true, false, "FanBoy");
-//    Human human3(false, true, false, "AverageMan");
-//    Human human4(false, false, true, "Marginal");
-//    Human human5(false, false, false, "Hater");
-
-    HumanBuilder humanBuilder;
-
-    humanBuilder.createHuman();
-    humanBuilder.setLovingAnime(true);
-    humanBuilder.setLovingHaha(false);
-    humanBuilder.setLovingPolit(false);
-    humanBuilder.setName("AnimeBoy");
-
-    Human* human1 = humanBuilder.getHuman();
-
-    humanBuilder.createHuman();
-    humanBuilder.setLovingAnime(true);
-    humanBuilder.setLovingHaha(true);
-    humanBuilder.setLovingPolit(false);
-    humanBuilder.setName("FanBoy");
-
-    Human* human2 = humanBuilder.getHuman();
-
-    humanBuilder.createHuman();
-    humanBuilder.setLovingAnime(false);
-    humanBuilder.setLovingHaha(true);
-    humanBuilder.setLovingPolit(false);
-    humanBuilder.setName("AverageBoy");
-
-    Human* human3 = humanBuilder.getHuman();
-
-    humanBuilder.createHuman();
-    humanBuilder.setLovingAnime(false);
-    humanBuilder.setLovingHaha(false);
-    humanBuilder.setLovingPolit(true);
-    humanBuilder.setName("Marginal");
-
-    Human* human4 = humanBuilder.getHuman();
-
-    humanBuilder.createHuman();
-    humanBuilder.setLovingAnime(false);
-    humanBuilder.setLovingHaha(false);
-    humanBuilder.setLovingPolit(false);
-    humanBuilder.setName("Hater");
-
-    Human* human5 = humanBuilder.getHuman();
-
-    LegendaryFactory *legendary_factory = new LegendaryFactory;
-    AnimeFactory *anime_factory = new AnimeFactory;
-    HahaFactory *haha_factory = new HahaFactory;
-    PolitFactory *polit_factory = new PolitFactory;
-
-    vector<Meme *> m;
-    m.push_back(legendary_factory->createMeme());
-    m.push_back(anime_factory->createMeme());
-    m.push_back(haha_factory->createMeme());
-    m.push_back(polit_factory->createMeme());
-
-    vector<Human> h;
-    h.push_back(*human1);
-    h.push_back(*human2);
-    h.push_back(*human3);
-    h.push_back(*human4);
-    h.push_back(*human5);
-
-    for (Human x : h) {
-        cout << x.getname() << endl;
-        for (Meme *y : m) {
-            x.review(*y);
-        }
-        cout << endl;
+    string s = " 5 + 2 ";
+    ExpressionParser n;
+    Calculator calculator;
+    list<string> expression;
+    try {
+        expression = n.parse(s);
+    } catch (logic_error e) {
+        cout << e.what() << endl; //некорректное выражение или скобки несогласованы
     }
+    bool flag = n.flag;
+    if (flag) {
+        for (string x : expression) { cout << x; } //for each печатает
+        cout << endl;
+        cout << calculator.calc(expression);
+    }
+
+    return 0;
 }
